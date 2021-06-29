d7ee72cbc9ffc83da2c30c24c0051f4c1e74c26c
Piotr Polanski <polanski.pio@gmail.com>
2021-06-29 22:14:27 +0900

    added game to rails

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,49 @@
+require 'open-uri'
+require 'json'
+
+class GamesController < ApplicationController
+  def new
+   @letters = generate_grid(10)
+   @now = Time.now
+   @start_time = @now.min * 60 + @now.sec
+  end
+
+  def score
+    @attempt = params[:attempt]
+    @end = Time.now
+    @end_time = @end.min * 60 + @end.sec
+    @letters = params[:letters].split("")
+    @start_time = params[:time].to_i
+    @score = calculate_score(@attempt, @start_time, @end_time)
+    @result = run_game(@attempt, @letters, @start_time, @end_time)
+  end
+
+  def generate_grid(grid_size)
+    Array.new(grid_size) { Array('A'..'Z').sample }
+  end
+
+  def word_info(attempt)
+    json = URI.open("https://wagon-dictionary.herokuapp.com/#{attempt}").read
+    JSON.parse(json)
+  end
+
+  def in_grid?(attempt, grid)
+    attempt.upcase.chars.all? { |letter| attempt.upcase.count(letter) <= grid.count(letter) }
+  end
+
+  def calculate_score(attempt, start_time, end_time)
+    if word_info(attempt)["length"] != nil
+      word_info(attempt)["length"] + (100 - (end_time - start_time))
+    end
+  end
+
+  def run_game(attempt, grid, start_time, end_time)
+    if in_grid?(attempt, grid) && word_info(attempt)["found"]
+      { time: end_time - start_time, score: @score, message: "Well done!" }
+    elsif in_grid?(attempt, grid) && !word_info(attempt)["found"]
+      { time: end_time - start_time, score: 0, message: "Not an English word" }
+    else
+      { time: end_time - start_time, score: 0, message: "Not in the grid" }
+    end
+  end
+end
